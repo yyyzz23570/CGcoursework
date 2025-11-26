9eb51275fac1ad79eecb89bfe7ebcf21310bb831
yyyzz23570 <tv23570@bristol.ac.uk>
2025-11-26 19:45:01 +0000

    Wireframe Render

--- RedNoise/src/RedNoise.cpp
+++ RedNoise/src/RedNoise.cpp
@@ -3,15 +3,11 @@
 #include <Utils.h>
 #include <CanvasPoint.h>
 #include <Colour.h>
-#include <TextureMap.h>
-#include <TexturePoint.h>
 #include <ModelTriangle.h>
 #include <fstream>
 #include <vector>
 #include <iostream>
 #include <cmath>
-#include <cstdlib>
-#include <ctime>
 #include <algorithm>
 #include <map>
 #include <sstream>
@@ -176,32 +172,31 @@ std::vector<ModelTriangle> loadOBJFile(const std::string &filename, const std::s
 	return triangles;
 }
 
-CanvasPoint projectPoint(glm::vec3 point, float focalLength = 2.0f) {
-	float cameraZ = -3.5f;
-	float offsetZ = point.z - cameraZ;
+CanvasPoint projectVertexOntoCanvasPoint(glm::vec3 cameraPosition, float focalLength, glm::vec3 vertexPosition) {
+	glm::vec3 vertexInCameraSpace = vertexPosition - cameraPosition;
 	
-	if (offsetZ <= 0) {
-		offsetZ = 0.1f;
+	float x = vertexInCameraSpace.x;
+	float y = vertexInCameraSpace.y;
+	float z = vertexInCameraSpace.z;
+	
+	if (z <= 0) {
+		z = 0.1f;
 	}
 	
-	float scale = focalLength / offsetZ;
-	float x = point.x * scale;
-	float y = point.y * scale;
+	float u = focalLength * (x / z) + WIDTH * 0.5f;
+	float v = focalLength * (y / z) + HEIGHT * 0.5f;
 	
-	float scaleFactor = 160.0f;
+	float imagePlaneScale = 160.0f;
 	
-	float screenX = x * scaleFactor + WIDTH * 0.5f;
-	float screenY = -y * scaleFactor + HEIGHT * 0.5f;
+	float offsetX = (u - WIDTH * 0.5f) * imagePlaneScale;
+	float offsetY = (v - HEIGHT * 0.5f) * imagePlaneScale;
 	
-	return CanvasPoint(screenX, screenY, point.z);
-}
-
-CanvasTriangle modelToCanvas(const ModelTriangle &triangle, float focalLength = 2.0f) {
-	CanvasPoint p0 = projectPoint(triangle.vertices[0], focalLength);
-	CanvasPoint p1 = projectPoint(triangle.vertices[1], focalLength);
-	CanvasPoint p2 = projectPoint(triangle.vertices[2], focalLength);
+	u = offsetX + WIDTH * 0.5f;
+	v = offsetY + HEIGHT * 0.5f;
 	
-	return CanvasTriangle(p0, p1, p2);
+	v = HEIGHT - v;
+	
+	return CanvasPoint(u, v, z);
 }
 
 void drawStrokedTriangle(DrawingWindow &window, const CanvasTriangle &triangle, const Colour &colour) {
@@ -210,224 +205,34 @@ void drawStrokedTriangle(DrawingWindow &window, const CanvasTriangle &triangle, 
 	drawLine(window, triangle[2], triangle[0], colour);
 }
 
-void drawFilledTriangle(DrawingWindow &window, const CanvasTriangle &triangle, const Colour &colour) {
-	CanvasPoint p0 = triangle[0];
-	CanvasPoint p1 = triangle[1];
-	CanvasPoint p2 = triangle[2];
-	
-	if (p0.y > p1.y) std::swap(p0, p1);
-	if (p0.y > p2.y) std::swap(p0, p2);
-	if (p1.y > p2.y) std::swap(p1, p2);
-	
-	uint32_t fillColour = (255 << 24) + (colour.red << 16) + (colour.green << 8) + colour.blue;
-	
-	int startY = (int)round(p0.y);
-	int endY = (int)round(p2.y);
-	
-	for (int y = startY; y <= endY; y++) {
-		float t;
-		float leftX, rightX;
-		
-		if (y <= p1.y) {
-			if (p1.y != p0.y) {
-				t = (y - p0.y) / (p1.y - p0.y);
-				leftX = p0.x + t * (p1.x - p0.x);
-			} else {
-				leftX = p0.x;
-			}
-			
-			if (p2.y != p0.y) {
-				t = (y - p0.y) / (p2.y - p0.y);
-				rightX = p0.x + t * (p2.x - p0.x);
-			} else {
-				rightX = p0.x;
-			}
-		} else {
-			if (p2.y != p1.y) {
-				t = (y - p1.y) / (p2.y - p1.y);
-				leftX = p1.x + t * (p2.x - p1.x);
-			} else {
-				leftX = p1.x;
-			}
-			
-			if (p2.y != p0.y) {
-				t = (y - p0.y) / (p2.y - p0.y);
-				rightX = p0.x + t * (p2.x - p0.x);
-			} else {
-				rightX = p0.x;
-			}
-		}
-		
-		if (leftX > rightX) std::swap(leftX, rightX);
-		
-		int startX = (int)round(leftX);
-		int endX = (int)round(rightX);
-		
-		for (int x = startX; x <= endX; x++) {
-			if (x >= 0 && x < window.width && y >= 0 && y < window.height) {
-				window.setPixelColour(x, y, fillColour);
-			}
-		}
-	}
+void drawWireframe(DrawingWindow &window) {
+	glm::vec3 cameraPosition(0.0f, 0.0f, -3.0f);
+	float focalLength = 2.0f;
 	
 	Colour white(255, 255, 255);
-	drawStrokedTriangle(window, triangle, white);
-}
-
-void drawTexturedTriangle(DrawingWindow &window, const CanvasTriangle &triangle, const TextureMap &texture) {
-	CanvasPoint p0 = triangle[0];
-	CanvasPoint p1 = triangle[1];
-	CanvasPoint p2 = triangle[2];
 	
-	if (p0.y > p1.y) std::swap(p0, p1);
-	if (p0.y > p2.y) std::swap(p0, p2);
-	if (p1.y > p2.y) std::swap(p1, p2);
-	
-	int startY = (int)round(p0.y);
-	int endY = (int)round(p2.y);
-	
-	for (int y = startY; y <= endY; y++) {
-		float t;
-		float leftX, rightX;
-		float leftTexX, leftTexY, rightTexX, rightTexY;
+	for (const auto &modelTriangle : modelTriangles) {
+		CanvasPoint p0 = projectVertexOntoCanvasPoint(cameraPosition, focalLength, modelTriangle.vertices[0]);
+		CanvasPoint p1 = projectVertexOntoCanvasPoint(cameraPosition, focalLength, modelTriangle.vertices[1]);
+		CanvasPoint p2 = projectVertexOntoCanvasPoint(cameraPosition, focalLength, modelTriangle.vertices[2]);
 		
-		if (y <= p1.y) {
-			if (p1.y != p0.y) {
-				t = (y - p0.y) / (p1.y - p0.y);
-				leftX = p0.x + t * (p1.x - p0.x);
-				leftTexX = p0.texturePoint.x + t * (p1.texturePoint.x - p0.texturePoint.x);
-				leftTexY = p0.texturePoint.y + t * (p1.texturePoint.y - p0.texturePoint.y);
-			} else {
-				leftX = p0.x;
-				leftTexX = p0.texturePoint.x;
-				leftTexY = p0.texturePoint.y;
-			}
-			
-			if (p2.y != p0.y) {
-				t = (y - p0.y) / (p2.y - p0.y);
-				rightX = p0.x + t * (p2.x - p0.x);
-				rightTexX = p0.texturePoint.x + t * (p2.texturePoint.x - p0.texturePoint.x);
-				rightTexY = p0.texturePoint.y + t * (p2.texturePoint.y - p0.texturePoint.y);
-			} else {
-				rightX = p0.x;
-				rightTexX = p0.texturePoint.x;
-				rightTexY = p0.texturePoint.y;
-			}
-		} else {
-			if (p2.y != p1.y) {
-				t = (y - p1.y) / (p2.y - p1.y);
-				leftX = p1.x + t * (p2.x - p1.x);
-				leftTexX = p1.texturePoint.x + t * (p2.texturePoint.x - p1.texturePoint.x);
-				leftTexY = p1.texturePoint.y + t * (p2.texturePoint.y - p1.texturePoint.y);
-			} else {
-				leftX = p1.x;
-				leftTexX = p1.texturePoint.x;
-				leftTexY = p1.texturePoint.y;
-			}
-			
-			if (p2.y != p0.y) {
-				t = (y - p0.y) / (p2.y - p0.y);
-				rightX = p0.x + t * (p2.x - p0.x);
-				rightTexX = p0.texturePoint.x + t * (p2.texturePoint.x - p0.texturePoint.x);
-				rightTexY = p0.texturePoint.y + t * (p2.texturePoint.y - p0.texturePoint.y);
-			} else {
-				rightX = p0.x;
-				rightTexX = p0.texturePoint.x;
-				rightTexY = p0.texturePoint.y;
-			}
-		}
+		CanvasTriangle canvasTriangle(p0, p1, p2);
 		
-		if (leftX > rightX) {
-			std::swap(leftX, rightX);
-			std::swap(leftTexX, rightTexX);
-			std::swap(leftTexY, rightTexY);
-		}
-		
-		int startX = (int)round(leftX);
-		int endX = (int)round(rightX);
-		
-		for (int x = startX; x <= endX; x++) {
-			if (x >= 0 && x < window.width && y >= 0 && y < window.height) {
-				float texT = (endX != startX) ? (x - startX) / (float)(endX - startX) : 0.0f;
-				float texX = leftTexX + texT * (rightTexX - leftTexX);
-				float texY = leftTexY + texT * (rightTexY - leftTexY);
-				
-				int texPixelX = (int)round(texX * (texture.width - 1));
-				int texPixelY = (int)round(texY * (texture.height - 1));
-				
-				texPixelX = std::max(0, std::min(texPixelX, (int)texture.width - 1));
-				texPixelY = std::max(0, std::min(texPixelY, (int)texture.height - 1));
-				
-				int pixelIndex = texPixelY * texture.width + texPixelX;
-				uint32_t textureColour = texture.pixels[pixelIndex];
-				
-				window.setPixelColour(x, y, textureColour);
-			}
-		}
+		drawStrokedTriangle(window, canvasTriangle, white);
 	}
-	
-	Colour white(255, 255, 255);
-	drawStrokedTriangle(window, triangle, white);
 }
 
 void draw(DrawingWindow &window) {
 	window.clearPixels();
-	
-	for (const auto &modelTriangle : modelTriangles) {
-		CanvasTriangle canvasTriangle = modelToCanvas(modelTriangle);
-		drawFilledTriangle(window, canvasTriangle, modelTriangle.colour);
-	}
+	drawWireframe(window);
 }
 
-void handleEvent(SDL_Event event, DrawingWindow &window, const TextureMap &texture) {
+void handleEvent(SDL_Event event, DrawingWindow &window) {
 	if (event.type == SDL_KEYDOWN) {
 		if (event.key.keysym.sym == SDLK_LEFT) std::cout << "LEFT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_RIGHT) std::cout << "RIGHT" << std::endl;
 		else if (event.key.keysym.sym == SDLK_UP) std::cout << "UP" << std::endl;
         else if (event.key.keysym.sym == SDLK_DOWN) std::cout << "DOWN" << std::endl;
-        else if (event.key.keysym.sym == SDLK_u) {
-        	CanvasPoint p0(rand() % window.width, rand() % window.height);
-        	CanvasPoint p1(rand() % window.width, rand() % window.height);
-        	CanvasPoint p2(rand() % window.width, rand() % window.height);
-        	CanvasTriangle tri(p0, p1, p2);
-        	Colour col(rand() % 256, rand() % 256, rand() % 256);
-        	drawStrokedTriangle(window, tri, col);
-        }
-        else if (event.key.keysym.sym == SDLK_f) {
-        	CanvasPoint p0(rand() % window.width, rand() % window.height);
-        	CanvasPoint p1(rand() % window.width, rand() % window.height);
-        	CanvasPoint p2(rand() % window.width, rand() % window.height);
-        	CanvasTriangle tri(p0, p1, p2);
-        	Colour col(rand() % 256, rand() % 256, rand() % 256);
-        	drawFilledTriangle(window, tri, col);
-        }
-        else if (event.key.keysym.sym == SDLK_t) {
-        	CanvasPoint p0(rand() % window.width, rand() % window.height);
-        	p0.texturePoint = TexturePoint(0.0, 0.0);
-        	
-        	CanvasPoint p1(rand() % window.width, rand() % window.height);
-        	p1.texturePoint = TexturePoint(1.0, 0.0);
-        	
-        	CanvasPoint p2(rand() % window.width, rand() % window.height);
-        	p2.texturePoint = TexturePoint(0.5, 1.0);
-        	
-        	CanvasTriangle tri(p0, p1, p2);
-        	drawTexturedTriangle(window, tri, texture);
-        }
-        else if (event.key.keysym.sym == SDLK_v) {
-        	CanvasPoint p0(160, 10);
-        	p0.texturePoint = TexturePoint(195.0/400.0, 5.0/400.0);
-        	
-        	CanvasPoint p1(300, 230);
-        	p1.texturePoint = TexturePoint(395.0/400.0, 380.0/400.0);
-        	
-        	CanvasPoint p2(10, 150);
-        	p2.texturePoint = TexturePoint(65.0/400.0, 330.0/400.0);
-        	
-        	CanvasTriangle tri(p0, p1, p2);
-        	drawTexturedTriangle(window, tri, texture);
-        	std::cout << "视觉验证测试三角形已绘制" << std::endl;
-        }
 	} else if (event.type == SDL_MOUSEBUTTONDOWN) {
 		window.savePPM("output.ppm");
 		window.saveBMP("output.bmp");
@@ -437,10 +242,6 @@ void handleEvent(SDL_Event event, DrawingWindow &window, const TextureMap &textu
 int main(int argc, char *argv[]) {
 	DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
 	SDL_Event event;
-    srand(static_cast<unsigned int>(time(nullptr)));
-    
-    TextureMap texture("texture.ppm");
-    std::cout << "纹理加载完成: " << texture.width << "x" << texture.height << std::endl;
 	
 	float scaleFactor = 0.35f;
 	modelTriangles = loadOBJFile("cornell-box.obj", "cornell-box.mtl", scaleFactor);
@@ -460,14 +261,8 @@ int main(int argc, char *argv[]) {
 	}
 	std::cout << "===================================\n" << std::endl;
 	
-	std::vector<float> result;
-	result = interpolateSingleFloats(2.2, 8.5, 7);
-	std::cout << "插值测试结果: ";
-	for(size_t i=0; i<result.size(); i++) std::cout << result[i] << " ";
-	std::cout << std::endl;
-	
 	while (true) {
-		if (window.pollForInputEvents(event)) handleEvent(event, window, texture);
+		if (window.pollForInputEvents(event)) handleEvent(event, window);
 		draw(window);
 		window.renderFrame();
 	}
